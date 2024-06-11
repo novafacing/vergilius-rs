c4bbb11ce3f1f8bee3718784dbfcaa70d826161f
novafacing <rowanbhart@gmail.com>
2024-06-11 11:57:26 -0700

    Add ignores/allows

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+#![allow(clippy::useless_transmute)]
 
 #[cfg(feature = "generate")]
 pub mod generate;
