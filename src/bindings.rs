0d26a8ca6df87bd46ca5f460794a422ed624dba0
novafacing <rowanbhart@gmail.com>
2024-06-11 10:37:34 -0700

    Add built headers

--- src/bindings.rs
+++ src/bindings.rs
@@ -8,10 +8,6 @@
 //! use vergilius::bindings::windows::*;
 //! ```
 
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-
 pub mod windows_10_0_10240_16384_x64;
 pub mod windows_10_0_10240_16384_x86;
 pub mod windows_10_0_10586_0_x64;
@@ -30,8 +26,8 @@ pub mod windows_10_0_18362_1_x86;
 pub mod windows_10_0_18362_30_x64;
 pub mod windows_10_0_18362_418_x64;
 pub mod windows_10_0_18362_418_x86;
-pub mod windows_10_0_19041_1288_x86;
 pub mod windows_10_0_19041_1288_x64;
+pub mod windows_10_0_19041_1288_x86;
 pub mod windows_10_0_19041_264_x64;
 pub mod windows_10_0_19041_264_x86;
 pub mod windows_10_0_19041_508_x64;
@@ -40,8 +36,8 @@ pub mod windows_10_0_19041_928_x64;
 pub mod windows_10_0_19041_928_x86;
 pub mod windows_10_0_19045_2965_x64;
 pub mod windows_10_0_19045_2965_x86;
-pub mod windows_10_0_22000_1_x64;
 pub mod windows_10_0_22000_194_x64;
+pub mod windows_10_0_22000_1_x64;
 pub mod windows_10_0_22621_382_x64;
 pub mod windows_10_0_22631_2428_x64;
 pub mod windows_5_1_2600_5512_x86;
